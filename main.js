93b86a66df182df49a4e61c0414f180744f39fb3
NaimisMe552 <160249490+NaimisMe552@users.noreply.github.com>
2024-08-02 20:20:28 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+document.getElementById('from').addEventListener('sumit', () => {
+    let userName = 'Naruto'
+
+    let password = 'alex@Naruto99'
+    
+    let user = document.getElementById('gmail').value;
+    let pass = document.getElementById('pass').value;
+    
+    if(userName === user){
+       document.getElementById('btn').a.href = #kali.html
+    }else{
+       alert('your password or username bot corract') 
+    }
+
+})
+
+
+
